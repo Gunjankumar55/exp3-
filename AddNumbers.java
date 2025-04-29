26c241d147aa6ddc806c4b150998c1edaebb05a5
Gunjankumar Choudhari <115869342+Gunjankumar55@users.noreply.github.com>
2025-04-29 23:33:09 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,8 +1,8 @@
-public class AdditionOfTwoNumbers {
+public class AddNumbers {
     public static void main(String[] args) {
         int num1 = 5;
-        int num2 = 7;
+        int num2 = 10;
         int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-    }
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
 }
